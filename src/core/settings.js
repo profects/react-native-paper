0f44c9df9656fc70403c233434484d81c4a978fa
Richard Lindhout <richardlindhout96@gmail.com>
2020-06-30 13:53:06 +0200

    Remove js files

--- src/core/settings.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import MaterialCommunityIcon from '../components/MaterialCommunityIcon';
-export const { Provider, Consumer } = React.createContext({
-    icon: MaterialCommunityIcon,
-});
